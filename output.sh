2a256479d65bc3eea82aa4c4cdbed35744125c08
mono121 <0n0dera0app@gmail.com>
2023-04-24 05:19:24 +0900

    change outpout.sh

--- output.sh
+++ output.sh
@@ -5,17 +5,26 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
+date_format="+%Y%m%d_%H%M"
 dir="$1"
+output="$(date ${date_format}).csv"
+
+# Create header for CSV file
+header="Directory,Extension,FileCount,DirectorySize,PercentageOfQuantity"
 
 # Find all directories in $dir and process each one
-find "$dir" -type d | while read -r subdir; do
+find "$dir" -type d | \
+while read -r subdir; do
     if [ "$(ls -A "$subdir")" ]; then  # Check if directory has files
-        printf "Directory: %s\n" "$subdir"
+        printf "Directory: %s\n" $(basename "$subdir")
         # Count the number of files, total file size, and number of files for each extension
-        find "$subdir" -type f | awk -F . '{if (NF>1) {print $NF} else {print "NoExtension"}}' | sort | uniq -c | awk '{printf "%s\t%s\t%s\n", $2, $1, $1/total*100}' total=$(find "$subdir" -type f | wc -l) | while read -r extension count percent; do
+        find "$subdir" -type f | awk -F . '{if (NF==2) {print $NF} else {print "NoExtension"}}' | sort | uniq -c | \
+        awk '{printf "%s\t%s\t%s\n", $2, $1, $1/total*100}' total=$(find "$subdir" -type f | wc -l) | \
+        while read -r extension count percent; do
             size=$(find "$subdir" -type f -name "*.$extension" -printf "%s\n" | awk '{sum += $1} END {print sum}')
-            printf "%-10s %5d %10d KB %5.2f%%\n" "$extension" "$count" "$((size/1024))" "$percent"
+            printf "%s,%-10s,%5d,%2d,%5.1f%%\n" $(basename "$subdir") "$extension" "$count" "$((size/1024))" "$percent"
         done
         printf "\n"
     fi
-done
+done | \
+awk 'BEGIN {print "'"$header"'"}; {print}' > "$output"
--- output_csv.sh
+++ output_csv.sh
@@ -9,14 +9,17 @@ dir="$1"
 output="$2"
 
 # Create header for CSV file
-header="Directory,Extension,Count,Size,Percent"
+header="Directory,Extension,FileCount,DirectorySize,Percent"
 
 # Find all directories in $dir and process each one
-find "$dir" -type d | while read -r subdir; do
+find "$dir" -type d | \
+while read -r subdir; do
     if [ "$(ls -A "$subdir")" ]; then  # Check if directory has files
         printf "Directory: %s\n" "$subdir"
         # Count the number of files, total file size, and number of files for each extension
-        find "$subdir" -type f | awk -F . '{if (NF>1) {print $NF} else {print "NoExtension"}}' | sort | uniq -c | awk '{printf "%s,%s,%s,%s\n", $2, $1, $1/total*100, ""}' total=$(find "$subdir" -type f | wc -l) | while read -r extension count percent _; do
+        find "$subdir" -type f | awk -F . '{if (NF>1) {print $NF} else {print "NoExtension"}}' | sort | uniq -c | \
+        awk '{printf "%s,%s,%s,%s\n", $2, $1, $1/total*100, ""}' total=$(find "$subdir" -type f | wc -l) | \
+        while read -r extension count percent _; do
             size=$(find "$subdir" -type f -name "*.$extension" -printf "%s\n" | awk '{sum += $1} END {print sum}')
             printf "%s,%s,%d,%d KB,%.2f%%\n" "$subdir" "$extension" "$count" "$((size/1024))" "$percent"
         done
